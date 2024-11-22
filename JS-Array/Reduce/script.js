5bfc2626a0330abfb271c9da9953e692e59116a8
SaifulBUBT <s.saifulbubt@gmail.com>
2024-11-22 22:34:57 +0600

    Array reduce method

---
+++ JS-Array/Reduce/script.js
@@ -0,0 +1,36 @@
+// 1) Basic Array reduce() method example
+
+const numbers = [1, 2, 3,4,5];
+
+const total = numbers.reduce(function(acc, currentValue){
+    return acc + currentValue;
+})
+console.log(total);
+
+
+// 2) Using reduce() method with an initial value
+
+const cart = [
+    {
+      product: 'phone',
+      qty: 1,
+      price: 500,
+    },
+    {
+      product: 'Screen Protector',
+      qty: 1,
+      price: 10,
+    },
+    {
+      product: 'Memory Card',
+      qty: 2,
+      price: 20,
+    },
+];
+
+function reducer(acc, currentItem){
+    return acc + (currentItem.qty * currentItem.price);
+}
+
+const cartTotal = cart.reduce(reducer, 0);
+console.log(cartTotal);
